fix pipeline and scaling

collators/default.py
@@ -26,7 +26,7 @@ class EncoderCollator:
     def item_to_arrays(item):
         prosody = Image.open(item["prosody"])
         prosody = np.array(prosody)
-        durs = prosody[-1]
+        durs = prosody[-1].copy()
         durs = (durs - durs.mean()) / (durs.std()+1e-6)
         cwt_result = EncoderCollator.compute_cwt(durs)
         cwt_result = (
@@ -193,28 +193,15 @@ class DecoderCollator:
         prosody, phones, speaker = EncoderCollator.item_to_arrays(item)
         duration = prosody[:, 30]
         # denormalize
-        duration = np.round(2**(duration * 8), 0).astype(np.int32)
-        mel = np.array(Image.open(item["mel"])).T
-        # TODO: this should not be necessary, investigate why this is happening
+        duration = np.ceil(2**(duration * 8)).astype(np.int32)
+        try:
+            mel = np.array(Image.open(item["mel"])).T
+        except:
+            mel = np.zeros((int(duration.sum()), 80))
+            print(f"Failed to load mel for {item['mel']}")
         if mel.shape[0] > duration.sum():
-            # add duration accross frames
-            duration_diff = mel.shape[0] - duration.sum()
-            duration_add = np.zeros(duration.shape[0], dtype=np.int32)
-            # ie if we need to add a third of the duration shape, duration add will be [1 , 0, 0 ,1 ,0 ,0 ...]
-            duration_add[::duration.shape[0] // duration_diff] = 1
-            rand_idx = np.random.randint(0, duration_add.shape[0])
-            if duration_add.sum() < duration_diff:
-                duration_add[rand_idx] += duration_diff - duration_add.sum()
-            elif duration_add.sum() > duration_diff:
-                duration_add[rand_idx] += duration_add.sum() - duration_diff
-        elif mel.shape[0] < duration.sum():
-            # pad mel
-            mel = np.pad(
-                mel,
-                ((0, duration.sum() - mel.shape[0]), (0, 0)),
-                mode="constant",
-                constant_values=0,
-            )
+            max_dur_idx = np.argmax(duration)
+            duration[max_dur_idx] += mel.shape[0] - duration.sum()
         # repeat prosody to match mel using duration
         prosody = np.repeat(prosody, duration, axis=0)
         # repeat speaker to match mel using duration

model/pipeline.py
@@ -76,12 +76,12 @@ class DDPMPipeline(DiffusionPipeline):
         mask = mask.to(self._device)
 
         if self.model_args.model_type == "decoder":
-            prosody_mask = torch.rand((batch_size, prosody_cond.shape[1], 1), device=self._device) < self.prosody_mask_prob
+            prosody_mask = torch.rand((batch_size, prosody_cond.shape[1], 1), device=self._device) > self.prosody_mask_prob
             prosody_cond = prosody_cond * prosody_mask
 
         for t in self.progress_bar(self.scheduler.timesteps):
 
-            image = self.scheduler.scale_model_input(image, t)
+            # image = self.scheduler.scale_model_input(image, t)
 
             # 1. predict noise model_output
             if self.model_args.model_type == "encoder":
@@ -120,8 +120,8 @@ class DDPMPipeline(DiffusionPipeline):
 
         image = image.cpu()
         if self.scale is not None:
-            image = torch.clamp(image, -self.scale, self.scale)
-            image = (image + self.scale) / (2 * self.scale)
+            # image = torch.clamp(image, -self.scale, self.scale)
+            image = image / self.scale
 
         return image
 

scripts/train.py
@@ -161,7 +161,7 @@ def train_epoch(epoch):
                     device=packed_prosody.device,
                 ).long()
                 if training_args.loss_type == "diffusion":
-                    packed_prosody = (packed_prosody * 2 - 1) * training_args.diffusion_scale
+                    packed_prosody = packed_prosody * training_args.diffusion_scale
                     noisy_ = noise_scheduler.add_noise(packed_prosody, noise, timesteps)
                     phone_mask = torch.rand(bsz, packed_phones.shape[1]) > training_args.phone_mask_prob
                     packed_phones = packed_phones * phone_mask
@@ -193,7 +193,7 @@ def train_epoch(epoch):
                     device=packed_mel.device,
                 ).long()
                 if training_args.loss_type == "diffusion":
-                    packed_mel = (packed_mel * 2 - 1) * training_args.diffusion_scale
+                    packed_mel = packed_mel * training_args.diffusion_scale
                     noisy_ = noise_scheduler.add_noise(packed_mel, noise, timesteps)
                     phone_mask = torch.rand(bsz, packed_phones.shape[1]) > training_args.phone_mask_prob
                     prosody_mask = torch.rand(bsz, packed_prosody.shape[1], 1) > training_args.prosody_mask_prob
@@ -399,7 +399,7 @@ def evaluate_loss_only():
                     device=packed_prosody.device,
                 ).long()
                 if training_args.loss_type == "diffusion":
-                    packed_prosody = (packed_prosody * 2 - 1) * training_args.diffusion_scale
+                    packed_prosody = packed_prosody * training_args.diffusion_scale
                     noisy_ = noise_scheduler.add_noise(packed_prosody, noise, timesteps)
                     output = model(
                         noisy_, packed_mask, timesteps, packed_phones, packed_speaker
@@ -429,7 +429,7 @@ def evaluate_loss_only():
                     device=packed_mel.device,
                 ).long()
                 if training_args.loss_type == "diffusion":
-                    packed_mel = (packed_mel * 2 - 1) * training_args.diffusion_scale
+                    packed_mel = packed_mel * training_args.diffusion_scale
                     noisy_ = noise_scheduler.add_noise(packed_mel, noise, timesteps)
                     output = model(
                         noisy_,