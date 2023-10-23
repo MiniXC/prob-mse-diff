79932c11d6105a73fab51f2849dd6912865ed089
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2023-10-23 10:37:47 +0000

    add silence to cer

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 if [ "$1" == "--machine" ] && [ "$2" == "v3-1" ]; then
     accelerate launch scripts/train.py \
-    --run_name decoder_diffusion_v3 \
+    --run_name decoder_diffusion_v4 \
     --wandb_mode online \
     --train_type decoder \
     --model_type decoder \
@@ -11,7 +11,7 @@ if [ "$1" == "--machine" ] && [ "$2" == "v3-1" ]; then
     --load_from_checkpoint checkpoints/decoder_diffusion_v3/temp
 elif [ "$1" == "--machine" ] && [ "$2" == "v3-2" ]; then
     accelerate launch scripts/train.py \
-    --run_name encoder_diffusion_v3 \
+    --run_name encoder_diffusion_v4 \
     --wandb_mode online \
     --train_type encoder \
     --model_type encoder \
@@ -22,13 +22,15 @@ elif [ "$1" == "--machine" ] && [ "$2" == "v3-2" ]; then
     --load_from_checkpoint checkpoints/encoder_diffusion_v3/temp
 elif [ "$1" == "--machine" ] && [ "$2" == "v3-3" ]; then
     accelerate launch scripts/train_byt5.py \
-    --run_name byt5_speaker_v3 \
+    --run_name byt5_speaker_v4 \
     --wandb_mode online \
-    --add_speaker_embedding
+    --add_speaker_embedding \
+    --load_from_checkpoint checkpoints/byt5_speaker_v3/temp
 elif [ "$1" == "--machine" ] && [ "$2" == "v3-4" ]; then
     accelerate launch scripts/train_byt5.py \
-    --run_name byt5_v3 \
-    --wandb_mode online
+    --run_name byt5_v4 \
+    --wandb_mode online \
+    --load_from_checkpoint checkpoints/byt5_v3/temp
 elif [ "$1" == "--test-decoder" ]; then
     /usr/bin/python scripts/train.py \
         --run_name decoder_diffusion_v3 \
